587a1dc70a8bbd6cf9894d0b1b8da850f185c877
BLealan <143086275+BLealan@users.noreply.github.com>
2023-12-04 17:49:44 +0000

    write logo file added

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const questions = require("./inquirerQuestions.js");
+const generateLogo = require("./shapeSVG.js");
+
+const writeToFile = (newFile) => 
+fs.writeFile("logo.svg", newFile, (err) => 
+err ? console.log(err): console.log("Generated logo.svg"))
 
 inquirer.prompt(questions)
-
 .then(data => {
     console.log(data)
-    // const logo = generateLogo(data)
+    const logo = generateLogo(data)
+    writeToFile(logo);
 })
 
 function init () {
--- shapeSVG.js
+++ shapeSVG.js
@@ -1,43 +1,46 @@
 class Shape {
-    constructor(width, height, backgroundColor, fillColor){
+    constructor(width, height, backgroundColor, shapeColor){
         this.width = 300;
         this.height = 200;
-        this.backgroundColor = "";
-        this.fillColor = ""
+        this.shapeColor = ""
     }
 };
 
 class Circle extends Shape {
-    constructor(width, height, backgroundColor, fillColor, shape) {
-        Shape.call(this, width, height, backgroundColor, fillColor);
-        this.shape = `<circle cx="25" cy="75" r="20"`;
+    constructor(width, height, shapeColor) {
+        Shape.call(this, width, height, shapeColor, shapeData);
+        this.shape = `circle cx="25" cy="75" r="75"`;
         ;
     }
 };
 
 class Triangle extends Shape {
-    constructor(width, height, backgroundColor, fillColor, shape) {
-        Shape.call(this, width, height, backgroundColor, fillColor);
-        this.shape = "Triangle";
+    constructor(width, height, shapeColor) {
+        Shape.call(this, width, height, shapeColor, shapeData);
+        this.shape = `polygon points="150, 25 175, 175 25, 175`;
     }
 };
 
 class Square extends Shape {
-    constructor(width, height, backgroundColor, fillColor, shape) {
-        Shape.call(this, width, height, backgroundColor, fillColor);
-        this.shape = 
+    constructor(width, height, shapeColor) {
+        Shape.call(this, width, height, shapeColor, shapeData);
+        this.shapeData = `rect x="10" y="10" width="150" height="150"`;
     }
 };
 
+const generateLogo = ({ width, height, shapeColor, shapeData, fontColor }) =>
 `<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
-        <svg version="1.1" width ="${shape.width}" height="${shape.height}" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill ="${shape.backgroundColor}/>
-        <text x="150" y="100" font-size="60" text-anchor="middle" fill="${fontColor}">TEST</text>
+        <svg version="1.1" width ="${Shape.width}" height="${Shape.height}" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill ="${Shape.shapeColor}"/>
+        <${Shape.shapeData}/>
+        <text x="150" y="100" font-size="60" text-anchor="middle" fill="${Shape.fontColor}">${Shape.characters}</text>
         </svg>
     </body>
 </html>`
+
+module.exports = generateLogo;
